848993b09576e4153da9a6d8f5d622e2d301daff
dgreeott <drKe.greeott@gmail.com>
2020-12-28 12:49:59 -0700

    About Started

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,19 +2,31 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='about'>
-           <h1>A Little About Me</h1>
-           <p>
-            My name is Zoë Mazurkiewicz and I'm currently working as 
-            an undergraduate student at Coe College. I'm double majoring in 
-            Political Science and Art. I moved to Iowa from Boulder, Colorado 
-            where I've lived most of my life. 
+       <div className="container-fluid about">
+           <div className='row'>
+                <h1>A Little About Me</h1>
+                <div className="col-sm">
+                <p>
+                My name is Zoë Mazurkiewicz and I'm currently working as 
+                an undergraduate student at Coe College. I'm double majoring in 
+                Political Science and Art. I moved to Iowa from Boulder, Colorado 
+                where I've lived most of my life. 
+                </p>
+                <p>
+                My favorite mediums are painting with acrylic and watercolor 
+                as well as sketching. I prefer two-dimensional work the most but 
+                I have experimented with sculpture classes which I found challenging 
+                but beneficial to my learning experience as an art student. 
+                </p> 
+        
 
-            My favorite mediums are painting with acrylic and watercolor 
-            as well as sketching. I prefer two-dimensional work the most but 
-            I have experimented with sculpture classes which I found challenging 
-            but beneficial to my learning experience as an art student. </p> 
-        </div>
+           </div>
+           <div className="col-sm">
+           </div>
+           </div>
+           
+       </div>
+       
     )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -49,17 +49,16 @@ class Contact extends Component {
 
   render() {
     return (
-      <div className="section">
-        <div className="container-fluid p-4">
-          <div className="row justify-content-center m-4">
-            <div className="container-fluid text-center">
-              <h1 className="title" id="contact">
-                CONTACT ME!
-              </h1>
-              <h6>I WILL GET BACK TO YOU AS SOON AS POSSIBLE!</h6>
-            </div>
-        <div className="container-fluid p-4">
-          <div className="row mt-5">
+      <div className="container-fluid p-4">
+        <div className="row justify-content-center m-4">
+          <div className="container-fluid text-center">
+            <h1 className="title" id="contact">
+              CONTACT ME!
+            </h1>
+            <h6>I WILL GET BACK TO YOU AS SOON AS POSSIBLE!</h6>
+          </div>
+          <div className="container-fluid w-50">
+            <div className="row mt-4">
               <div className="col-sm text-center">
                 <form
                   id="contactForm"
@@ -111,7 +110,6 @@ class Contact extends Component {
             </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
